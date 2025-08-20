11a3102ccc6faf637a1c2ac2cd6e2fc29759ec33
joemcerlane <joemcerlane@gmail.com>
2025-08-20 15:01:32 -0700

    removing unneeded text

--- src/components/Education.js
+++ src/components/Education.js
@@ -34,7 +34,6 @@ const Education = () => (
   <section className="py-10 md:py-16">
     <div className="container max-w-screen-xl mx-auto px-4">
       <h1 className="font-medium text-gray-700 text-3xl md:text-4xl mb-5">Education</h1>
-      <p className="font-normal text-gray-500 text-xs md:text-base mb-20">A summary of the places I had the opportunity to study</p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {educationItems.map((item, idx) => (
           <div key={idx} className="bg-gray-50 px-8 py-10 rounded-md">
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -15,7 +15,6 @@ const SkillsChart = () => (
   <section className="py-10 md:py-16">
     <div className="container max-w-screen-xl mx-auto px-4">
       <h1 className="font-medium text-gray-700 text-3xl md:text-4xl mb-5">Skills Chart</h1>
-      <p className="font-normal text-gray-500 text-xs md:text-base mb-20">A summary of my technical skills</p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {skills.map((skill, idx) => (
           <div key={idx} className="bg-gray-50 px-8 py-10 rounded-md">
