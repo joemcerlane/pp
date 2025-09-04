ef5f1a20a429832e6c66a6348dc365568e4640a1
joemcerlane <joemcerlane@gmail.com>
2025-09-03 22:17:28 -0700

    removing unneeded here

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => (
     <div className="container max-w-screen-xl mx-auto px-4">
       <div className="text-center">
         <h1 className="font-medium text-gray-700 text-4xl md:text-5xl mb-5">Contact</h1>
-        <p className="font-normal text-gray-400 text-md md:text-lg mb-20">I’m open to new opportunities and collaborations reach me here <a href="mailto:joemcerlane@gmail.com" className="text-gray-600 hover:underline">here</a>.</p>
+        <p className="font-normal text-gray-400 text-md md:text-lg mb-20">I’m open to new opportunities and collaborations reach me <a href="mailto:joemcerlane@gmail.com" className="text-gray-600 hover:underline">here</a>.</p>
       </div>
     </div>
   </footer>
